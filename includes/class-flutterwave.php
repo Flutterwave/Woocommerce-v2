047e325dc63887f731ca6dcbc80d9871ad9778c5
Olaobaju Abraham <olaobajua@gmail.com>
2023-05-20 17:37:37 +0100

    update language textdomain function

--- includes/class-flutterwave.php
+++ includes/class-flutterwave.php
@@ -86,10 +86,6 @@ final class Flutterwave {
 	public function load_plugin_textdomain() {
 		$locale = determine_locale();
 
-		/**
-		 * Filter to adjust the WooCommerce locale to use for translations.
-		 */
-		$locale = apply_filters( 'plugin_locale', $locale, 'rave-woocommerce-payment-gateway' ); // phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingSinceComment
 		load_plugin_textdomain( 'rave-woocommerce-payment-gateway', false, dirname( FLW_WC_PLUGIN_BASENAME ) . '/i18n/languages' );
 	}
 
