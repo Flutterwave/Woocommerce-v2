5e89fbe34d8ac6a4f9a6b89de68a7d5b134e264f
bajoski34 <olaobajua@gmail.com>
2026-09-14 19:42:49 +0100

    update ci tests

--- bin/run-ci-tests.bash
+++ bin/run-ci-tests.bash
@@ -8,7 +8,7 @@ IFS=$'\n\t'
 WOOCOMMERCE_RAVE_DIR="$GITHUB_WORKSPACE"
 
 echo 'Updating composer version & Install dependencies...'
-composer self-update 2.0.6 && composer install --no-progress
+composer install --no-progress
 
 echo 'Starting MySQL service...'
 sudo systemctl start mysql.service
